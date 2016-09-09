4be4cd0d28693bfb312d22d8b9d4f1653e9d0ca3
Evan Stewart <stewarea@gmail.com>
2016-09-09 10:29:11 -0700

    add contact info to new contact page and add styling to list items

--- app/app.php
+++ app/app.php
@@ -25,7 +25,7 @@
     });
     $app->post("/delete_contacts", function() use ($app) {
             Contact::deleteContacts();
-            return $app['twig']->render('home.html.twig');
+            return $app['twig']->render('clear_contacts.html.twig');
     });
 
     return $app;
